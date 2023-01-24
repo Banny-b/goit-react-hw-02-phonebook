7bb095afd0a16a8d758b935c2cd361cd2af73a2c
Sergey Petukhov <banny75@i.ua>
2023-01-24 14:34:17 +0200

    Update ContactForm.jsx

--- src/components/Phonebook/ContactForm/ContactForm.jsx
+++ src/components/Phonebook/ContactForm/ContactForm.jsx
@@ -45,7 +45,7 @@ export default class ContactForm extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            placeholder="Rosie Simpson"
+            placeholder="Enter your name"
           />
         </label>
 
@@ -59,7 +59,7 @@ export default class ContactForm extends Component {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-            placeholder="645-17-79"
+            placeholder="Enter your phone number"
           />
         </label>
 
